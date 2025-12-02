a4172b425fda29bce0f7f7ea9603064253f23480
Mrafro32 <153801840+Mrafro32@users.noreply.github.com>
2025-12-02 09:06:11 -0600

    Add comments to NodoPago class attributes and constructor

--- NodoPago.java
+++ NodoPago.java
@@ -1,9 +1,9 @@
 public class NodoPago {
-    double monto;
-    String fecha;
-    NodoPago siguiente;
+    double monto; //monto del pago reaalizado
+    String fecha; //fecha en la que se hizo el pago
+    NodoPago siguiente; // referencia al siguiente nodo de pago
 
-    public NodoPago(double monto, String fecha){
+    public NodoPago(double monto, String fecha){ // constructor que inicializa el pago
         this.monto = monto;
         this.fecha = fecha;
         this.siguiente = null;
