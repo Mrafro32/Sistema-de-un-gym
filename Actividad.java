9496fe49d5dd5caab843afe3a6f1705ab0cab3ec
Mrafro32 <153801840+Mrafro32@users.noreply.github.com>
2025-12-02 09:24:44 -0600

    Update Actividad.java

--- Actividad.java
+++ Actividad.java
@@ -1,11 +1,13 @@
 public class Actividad {
-    public ColaUsuario cola;
-    public NodoEntrenador entrenador;
-    public String nombre;
+    ColaUsuario cola; // cola circular para gestionar usuarios inscritos
+    NodoEntrenador entrenador; // entrenador asignado a la actividad
+    String nombre; //nombre de la actividad
+    int capacidad; // capacidad max de participantes
 
-    public Actividad(String nombre, int capacidad){
-        this.nombre = nombre;
-        this.cola = new ColaUsuario(capacidad);
-        this.entrenador = null;
+    public Actividad(String nombre, int capacidad){ //constructor para inicializar la actividad con nombre y capacidad
+        this.nombre = nombre; 
+        this.cola = new ColaUsuario(capacidad);// inicializa la cola con la capacidad
+        this.entrenador = null; //inicialmente no tiene entrenador asignado
+        this.capacidad = capacidad;
     }
 }
