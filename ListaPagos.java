2a6ec7ebedfb874f1f5b877f945e832f6942cd28
Mrafro32 <153801840+Mrafro32@users.noreply.github.com>
2025-12-02 09:08:25 -0600

    Update ListaPagos.java

--- ListaPagos.java
+++ ListaPagos.java
@@ -1,22 +1,22 @@
 public class ListaPagos {
-    NodoPago cabeza;
+    NodoPago cabeza; //primer nodo de la lista
     
-    public ListaPagos(){
+    public ListaPagos(){ // constructor vacio
         cabeza = null;
     }
-    public void agregarPago(double monto, String fecha){
+    public void agregarPago(double monto, String fecha){ // metodo para agregar un nuevo pago al final de lista
         NodoPago nuevo = new NodoPago(monto, fecha);
         if(cabeza == null){
-            cabeza = nuevo;
+            cabeza = nuevo; // si la lista esta vacia, el nuevo nodo es la cabeza
         }else{
             NodoPago temp = cabeza;
             while(temp.siguiente != null){
-                temp = temp.siguiente;
+                temp = temp.siguiente; // recorre hasta el ultimo nodo
             }
-            temp.siguiente = nuevo;
+            temp.siguiente = nuevo; // agrega el nuevo nodo al final
         }
     }
-    public void mostrarPago(){
+    public void mostrarPago(){ // metodo para mostrar pagos de la lista
         NodoPago temp = cabeza;
         while(temp != null){
             System.out.println("Monto: " + temp.monto + "Fecha: " + temp.fecha);
