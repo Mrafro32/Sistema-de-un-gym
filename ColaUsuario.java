027d3b9be635234a84435acc0e64472898f4ecb3
Mrafro32 <153801840+Mrafro32@users.noreply.github.com>
2025-12-02 09:28:17 -0600

    Update ColaUsuario.java

--- ColaUsuario.java
+++ ColaUsuario.java
@@ -1,10 +1,10 @@
 import javax.swing.JTextArea;
 
 public class ColaUsuario {
-    private Usuario[] cola;
-    private int frente, fin, tamaño, capacidad;
+    private Usuario[] cola; //arreglo que representa la cola
+    private int frente, fin, tamaño, capacidad; //variable de control de la cola
 
-    public ColaUsuario(int capacidad){
+    public ColaUsuario(int capacidad){ //constructor que defina la capacidad max de la cola
         this.capacidad = capacidad;
         cola = new Usuario[capacidad];
         frente = 0;
@@ -12,15 +12,15 @@ public class ColaUsuario {
         tamaño = 0;
     }
 
-    public boolean enqueue(Usuario u){
-        if(tamaño == capacidad) return false;
+    public boolean enqueue(Usuario u){ // agregar un usuario al final de la cola
+        if(tamaño == capacidad) return false; // no se puede agregar si la cola esta llena 
         cola[fin] = u;
-        fin = (fin + 1) % capacidad;
+        fin = (fin + 1) % capacidad; //mueve el indice de fin circularmente 
         tamaño++;
         return true;
     }
 
-    public void eliminarUsuario(int id){
+    public void eliminarUsuario(int id){ // elimina el usuario por ID
         if(tamaño == 0) return;
         int cont = 0;
         Usuario[] nueva = new Usuario[capacidad];
@@ -38,7 +38,7 @@ public class ColaUsuario {
         tamaño = cont;
     }
 
-    public void mostrarColaGUI(JTextArea area){
+    public void mostrarColaGUI(JTextArea area){ //muestra los usuarios en la GUI
         if(tamaño == 0){
             area.append("No hay inscritos.\n");
             return;
