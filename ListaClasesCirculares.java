0fcbac0e4fc6e165554a37ec77fb11685b437a40
Mrafro32 <153801840+Mrafro32@users.noreply.github.com>
2025-12-02 09:37:43 -0600

    Add comments to ListaClasesCirculares methods

--- ListaClasesCirculares.java
+++ ListaClasesCirculares.java
@@ -1,31 +1,31 @@
 public class ListaClasesCirculares {
-    NodoClase cabeza;
+    NodoClase cabeza; //referencia al primer nodo de la lista
 
     public ListaClasesCirculares(){
-        cabeza = null;
+        cabeza = null; //inicialmente la lista esta vacia
     }
-    public void agregarClase(String nombre){
+    public void agregarClase(String nombre){ // metodo para agregar una nueva clase a la lista circular
         NodoClase  nuevo = new NodoClase(nombre);
-        if(cabeza == null){
+        if(cabeza == null){ // si la lista esta vacia, el nuevo nodo apunta a si mismo
             cabeza = nuevo;
             cabeza.siguiente = cabeza;
-        }else{
+        }else{ // si hay nodos, se recirre hasta el ultimo y se inserta el nuevo al final 
             NodoClase temp = cabeza;
-            while(temp.siguiente != cabeza){
+            while(temp.siguiente != cabeza){ //recorrer hasta llegarl al ultimo nodo
                 temp = temp.siguiente;
             }
-            temp.siguiente = nuevo;
-            nuevo.siguiente = cabeza;
+            temp.siguiente = nuevo; //apuntar el ultimo nodo al nuevo
+            nuevo.siguiente = cabeza; //el nuevo nodo cierra el ciruclo apuntando a la cabeza
         }
     }
-    public void mostrarClases(int vueltas){
+    public void mostrarClases(int vueltas){ //metodo para mostrar clases en la lista circular
         NodoClase temp = cabeza;
-        if(cabeza == null) return;
+        if(cabeza == null) return; // si la lista esta vacia no se hace nada
         for(int i = 0; i < vueltas; i++){
             do{
                 System.out.println("Clase: " + temp.nombre);
                 temp = temp.siguiente;
-            } while(temp != cabeza);
+            } while(temp != cabeza); //recorre hsata voler al nodo inicial
         }
     }
 }
